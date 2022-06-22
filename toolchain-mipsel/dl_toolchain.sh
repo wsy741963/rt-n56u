6396566aec942679ed56aae66365debe24961305
wsy741963 <wsy741963@live.com>
2022-06-22 10:10:43 +0800

    Update dl_toolchain.sh

--- toolchain-mipsel/dl_toolchain.sh
+++ toolchain-mipsel/dl_toolchain.sh
@@ -2,7 +2,7 @@
 
 DIR="toolchain-3.4.x"
 DL_NAME="mipsel-linux-uclibc.tar.xz"
-DL_URL="https://hub.xn--gzu630h.xn--kpry57d/hanwckf/padavan-toolchain/releases/download/v1.1/$DL_NAME"
+DL_URL="https://github.com/hanwckf/padavan-toolchain/releases/download/v1.1/$DL_NAME"
 
 if [ -d $DIR ]; then
 	echo "$DIR exists!"
